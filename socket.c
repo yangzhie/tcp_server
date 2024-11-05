b6762cde20db5b9364750bb221783da3928217fb
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-11-05 16:58:14 +1100

    Feat: fill socket addrs in structure.

--- socket.c
+++ socket.c
@@ -3,14 +3,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define IP "69.16.231.59" /* www.gooogle.se */
 #define PORT 80           /* HTTP */
 
 int main()
 {
-    int s;             // Socket return value
-    sockaddr_in sock; // Store the socket IP address
+    int s;                   // Socket return value
+    struct sockaddr_in sock; // Store the socket IP address
 
     /* Init socket */
     // Make a call for the socket
@@ -21,4 +22,9 @@ int main()
         printf("socket() error.\n");
         return -1;
     };
+
+    /* Fill socket address in structure */
+    sock.sin_addr.s_addr = inet_addr(IP); // Store IP address of the target
+    sock.sin_port = htons(PORT);          // Put in port, htons() converts to network byte order
+    sock.sin_family = AF_INET;            // Address family
 }
